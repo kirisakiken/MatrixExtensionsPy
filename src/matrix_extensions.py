d4119c5eba06e3b7eda06c49ee99dcf3d59889eb
Bezmican Zehir <bezmican@aifi.com>
2022-05-28 16:52:14 +0300

    feat(matrix-extensions): add matrix extensions base

---
+++ src/matrix_extensions.py
@@ -0,0 +1,8 @@
+
+class MatrixExtensions:
+    def __init__(self):
+        pass
+        # self.translation_operator = TranslationOperator()
+        # self.rotation_operator = RotationOperator()
+        # self.scale_operator = ScaleOperator()
+        # self.transformation_operator = TransformationOperator()
