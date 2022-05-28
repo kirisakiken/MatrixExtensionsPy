bf5d7f650ef7c3341df60ff9a686d4e0ae21d437
Bezmican Zehir <bezmican@aifi.com>
2022-05-28 21:48:50 +0300

    refactor: made available methods private in operators

--- src/operations/rotation.py
+++ src/operations/rotation.py
@@ -12,13 +12,13 @@ class RotationOperator:
         pass
 
     @staticmethod
-    def get_rotation_matrix_2d(degree: float):
+    def __get_rotation_matrix_2d(degree: float):
         rad = math.radians(degree)
         return np.array([[math.cos(rad), -math.sin(rad)],
                          [math.sin(rad), math.cos(rad)]])
 
     @staticmethod
-    def get_rotation_matrix_3d(degree: float, axis: AXIS):
+    def __get_rotation_matrix_3d(degree: float, axis: AXIS):
         rad = math.radians(degree)
         if axis == AXIS.X:
             return np.array([[1, 0, 0, 0],
@@ -39,15 +39,15 @@ class RotationOperator:
             raise SyntaxError(f"{AXIS.name} is out of range")
 
     def get_rotation_2d(self, vector_2: Vector2, degree: float) -> Vector2:
-        rotation_matrix = self.get_rotation_matrix_2d(degree)
+        rotation_matrix = self.__get_rotation_matrix_2d(degree)
         vector_2_matrix = VectorExtensions.vector2_to_matrix_1x2(vector_2)
         result = np.matmul(rotation_matrix, vector_2_matrix)
         return Vector2(result[0][0], result[1][0])
 
     def get_rotation_3d(self, vector_3: Vector3, rotation_vector_3: Vector3) -> Vector3:
-        rotation_matrix_x = self.get_rotation_matrix_3d(rotation_vector_3.x, AXIS.X)
-        rotation_matrix_y = self.get_rotation_matrix_3d(rotation_vector_3.y, AXIS.Y)
-        rotation_matrix_z = self.get_rotation_matrix_3d(rotation_vector_3.z, AXIS.Z)
+        rotation_matrix_x = self.__get_rotation_matrix_3d(rotation_vector_3.x, AXIS.X)
+        rotation_matrix_y = self.__get_rotation_matrix_3d(rotation_vector_3.y, AXIS.Y)
+        rotation_matrix_z = self.__get_rotation_matrix_3d(rotation_vector_3.z, AXIS.Z)
 
         vector_3_matrix = VectorExtensions.vector3_to_matrix_1x4(vector_3)
 
--- src/operations/transformation.py
+++ src/operations/transformation.py
@@ -15,23 +15,23 @@ class TransformationOperator:
         pass
 
     @staticmethod
-    def get_transformation_matrix_2d(transformation_axis: Axis2):
+    def __get_transformation_matrix_2d(transformation_axis: Axis2):
         return AxisExtensions.axis2_to_matrix_2x2(transformation_axis)
 
     @staticmethod
-    def get_transformation_matrix_3d(transformation_axis: Axis3):
+    def __get_transformation_matrix_3d(transformation_axis: Axis3):
         return AxisExtensions.axis3_to_matrix_3x3(transformation_axis)
 
     # linear transformation 2D
     def get_transformation_2d(self, vector_2: Vector2, transformation_axis: Axis2) -> Vector2:
         position_matrix = VectorExtensions.vector2_to_matrix_1x2(vector_2)
-        transformation_matrix = self.get_transformation_matrix_2d(transformation_axis)
+        transformation_matrix = self.__get_transformation_matrix_2d(transformation_axis)
         result = np.matmul(transformation_matrix, position_matrix)
         return Vector2(result[0][0], result[1][0])
 
     # linear transformation 3D
     def get_transformation_3d(self, vector_3: Vector3, transformation_axis: Axis3) -> Vector3:
         position_matrix = VectorExtensions.vector3_to_matrix_1x3(vector_3)
-        transformation_matrix = self.get_transformation_matrix_3d(transformation_axis)
+        transformation_matrix = self.__get_transformation_matrix_3d(transformation_axis)
         result = np.matmul(transformation_matrix, position_matrix)
         return Vector3(result[0][0], result[1][0], result[2][0])
