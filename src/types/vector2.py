7b3d69e70c197c7ef4850036eb190ad5822ad024
kirisakiken <bezmican96@gmail.com>
2022-06-01 22:48:15 +0300

    feat(vector): add math operator overloads

--- src/types/vector2.py
+++ src/types/vector2.py
@@ -14,6 +14,25 @@ class Vector2:
     def one():
         return Vector2(1, 1)
 
+    # region Operators
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector2(self.x * other.x, self.y * other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not (self.x == other.x and self.y == other.y)
+
+    # endregion
+
     def get_magnitude(self):
         return math.sqrt((self.x ** 2) + (self.y ** 2))
 
--- src/types/vector3.py
+++ src/types/vector3.py
@@ -15,6 +15,25 @@ class Vector3:
     def one():
         return Vector3(1, 1, 1)
 
+    # region Operators
+
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, other):
+        return Vector3(self.x * other.x, self.y * other.y, self.z * other.z)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __ne__(self, other):
+        return not (self.x == other.x and self.y == other.y and self.z == other.z)
+
+    # endregion
+
     def get_magnitude(self):
         return math.sqrt((self.x ** 2) + (self.y ** 2) + (self.z ** 2))
 
@@ -24,4 +43,3 @@ class Vector3:
             return Vector3.zero()
 
         return Vector3(self.x / v_len, self.y / v_len, self.z / v_len)
-
